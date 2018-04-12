4f1d249787ba5174620127099ccaa87bdcbc3f49
Daichi Hioki <hiokidaichi@gmail.com>
2018-04-12 09:42:24 +0900

    Add GET /ping endpoint

---
+++ app/Main.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import           Web.Spock
+import           Web.Spock.Config
+
+import           Data.IORef
+
+data MySession =
+  EmptySession
+
+newtype MyAppState =
+  DummyAppState (IORef Int)
+
+main :: IO ()
+main = do
+  ref <- newIORef 0
+  spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
+  runSpock 8080 (spock spockCfg app)
+
+app :: SpockM () MySession MyAppState ()
+app = get "ping" $ text "pong"
