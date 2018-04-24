11ee6cbb414c0855fd828fc471aae59bc120c6c4
Daichi Hioki <hiokidaichi@gmail.com>
2018-04-24 23:38:05 +0900

    Implement GET /books, GET /books/:id

--- src/Web/Flashcard.hs
+++ src/Web/Flashcard.hs
@@ -2,9 +2,12 @@
 
 module Web.Flashcard where
 
-import           Control.Monad.IO.Class (liftIO)
-import           Data.IORef             (IORef, newIORef)
+import           Control.Monad.IO.Class    (liftIO)
+import           Data.IORef                (IORef, newIORef)
+import           GHC.Int
+import           Model.Book
 import           Model.Health
+import           Network.HTTP.Types.Status
 import           Web.Spock
 import           Web.Spock.Config
 
@@ -15,11 +18,21 @@ newtype MyAppState =
   DummyAppState (IORef Int)
 
 app :: SpockM () MySession MyAppState ()
-app =
+app = do
   get "ping" $ do
     setCommonHeader
     health <- liftIO touchHealth
     json health
+  get "books" $ do
+    setCommonHeader
+    books <- liftIO getBooks
+    json books
+  get ("books" <//> (var :: Var Int64)) $ \bookId -> do
+    setCommonHeader
+    mBook <- liftIO $ getBook bookId
+    case mBook of
+      Just book -> json book
+      Nothing   -> setStatus status404
   where
     setCommonHeader :: ActionCtxT ctx (WebStateM () MySession MyAppState) ()
     setCommonHeader = setHeader "Access-Control-Allow-Origin" "http://localhost:3000"
