00daba4c4a80ca718d39705c81e0272dc51939b8
Daichi Hioki <hiokidaichi@gmail.com>
2018-05-03 17:32:22 +0900

    Use prehook

--- src/Web/Flashcard.hs
+++ src/Web/Flashcard.hs
@@ -10,8 +10,8 @@ import           Model.Book                (getBook, getBooks)
 import           Model.Health              (touchHealth)
 import           Model.Page                (getPagesByBookId)
 import           Network.HTTP.Types.Status (status404)
-import           Web.Spock                 (SpockAction, SpockM, Var, get, json, runSpock, setHeader, setStatus, spock,
-                                            var, (<//>))
+import           Web.Spock                 (SpockAction, SpockM, Var, get, json, prehook, runSpock, setHeader,
+                                            setStatus, spock, var, (<//>))
 import           Web.Spock.Config          (PoolOrConn (PCNoDatabase), defaultSpockCfg)
 
 type Api = SpockM () () () ()
@@ -19,25 +19,22 @@ type Api = SpockM () () () ()
 type ApiAction a = SpockAction () () () a
 
 app :: Api
-app = do
-  get "ping" $ do
-    setCommonHeader
-    health <- liftIO touchHealth
-    json health
-  get "books" $ do
-    setCommonHeader
-    books <- liftIO getBooks
-    json books
-  get ("books" <//> (var :: Var Int64)) $ \bookId -> do
-    setCommonHeader
-    mBook <- liftIO $ getBook bookId
-    case mBook of
-      Just book -> json book
-      Nothing   -> setStatus status404
-  get ("books" <//> (var :: Var Int64) <//> "pages") $ \bookId -> do
-    setCommonHeader
-    pages <- liftIO $ getPagesByBookId bookId
-    json pages
+app =
+  prehook setCommonHeader $ do
+    get "ping" $ do
+      health <- liftIO touchHealth
+      json health
+    get "books" $ do
+      books <- liftIO getBooks
+      json books
+    get ("books" <//> (var :: Var Int64)) $ \bookId -> do
+      mBook <- liftIO $ getBook bookId
+      case mBook of
+        Just book -> json book
+        Nothing   -> setStatus status404
+    get ("books" <//> (var :: Var Int64) <//> "pages") $ \bookId -> do
+      pages <- liftIO $ getPagesByBookId bookId
+      json pages
   where
     setCommonHeader :: ApiAction ()
     setCommonHeader = setHeader "Access-Control-Allow-Origin" "http://localhost:4000"
